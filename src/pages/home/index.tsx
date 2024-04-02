bf855c1af5d13759758944b9ba836a3922ded39b
moreshwarnabar <mrnabar@gmail.com>
2024-04-01 19:32:44 -0700

    added latest-activities to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,9 @@
-import { DealsChart, TotalCountCard, UpcomingEvents } from '@/components';
+import {
+  DealsChart,
+  LatestActivities,
+  TotalCountCard,
+  UpcomingEvents,
+} from '@/components';
 import { DASHBOARD_TOTAL_COUNTS_QUERY } from '@/graphql/queries';
 import { DashboardTotalCountsQuery } from '@/graphql/types';
 import { useCustom } from '@refinedev/core';
@@ -44,6 +49,12 @@ export const Home = () => {
         </Col>
         <Col xs={24} sm={24} xl={16} style={{ height: '460px' }}>
           <DealsChart />
+        </Col>
+      </Row>
+
+      <Row gutter={[32, 32]} style={{ marginTop: '32px' }}>
+        <Col xs={24}>
+          <LatestActivities />
         </Col>
       </Row>
     </div>
