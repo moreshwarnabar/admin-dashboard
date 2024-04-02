6303d4d3621f4828d8b84f380d82b95be8477c9e
moreshwarnabar <mrnabar@gmail.com>
2024-04-01 18:10:19 -0700

    added export for upcoming events skeleton

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 import UpcomingEvents from './home/upcoming-events';
 import DealsChart from './home/deals-chart';
 
-export { UpcomingEvents, DealsChart };
+import UpcomingEventsSkeleton from './skeleton/upcoming-events';
+
+export { UpcomingEvents, DealsChart, UpcomingEventsSkeleton };
