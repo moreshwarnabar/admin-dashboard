5ce6b5838d9fe0384b897214bff8ed1ab20e567f
moreshwarnabar <mrnabar@gmail.com>
2024-04-02 11:43:11 -0700

    added style for kanban board container

--- src/components/tasks/kanban/board.tsx
+++ src/components/tasks/kanban/board.tsx
@@ -1,7 +1,31 @@
-export const KanbanBoardContainer = () => {
-  return <div>KanbanBoardContainer</div>;
+import { DndContext } from '@dnd-kit/core';
+
+export const KanbanBoardContainer = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div
+      style={{
+        width: 'calc(100% + 64px)',
+        height: 'calc(100vh - 64px)',
+        display: 'flex',
+        flexDirection: 'column',
+        margin: '-32px',
+      }}
+    >
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          padding: '32px',
+          overflow: 'scroll',
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
 };
 
-export const KanbanBoard = () => {
-  return <div>KanbanBoard</div>;
+export const KanbanBoard = ({ children }: React.PropsWithChildren) => {
+  return <DndContext>{children}</DndContext>;
 };
