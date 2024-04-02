0a3408e1eebe142c69ea9b66ed882cd5435d5d27
moreshwarnabar <mrnabar@gmail.com>
2024-04-01 19:06:15 -0700

    exported latest-activities compoment

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,14 @@
 import UpcomingEvents from './home/upcoming-events';
 import DealsChart from './home/deals-chart';
 import TotalCountCard from './home/total-count-card';
+import LatestActivities from './home/latest-activities';
 
 import UpcomingEventsSkeleton from './skeleton/upcoming-events';
 
-export { UpcomingEvents, DealsChart, TotalCountCard, UpcomingEventsSkeleton };
+export {
+  UpcomingEvents,
+  DealsChart,
+  TotalCountCard,
+  LatestActivities,
+  UpcomingEventsSkeleton,
+};
