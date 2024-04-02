1b04f5e2e6d7d21a6e67f4127c042e53926d740d
moreshwarnabar <mrnabar@gmail.com>
2024-04-02 12:04:04 -0700

    added basic structure for kanban column

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -3,12 +3,15 @@ import {
   KanbanBoard,
 } from '@/components/tasks/kanban/board';
 import KanbanColumn from '@/components/tasks/kanban/column';
+import KanbanItem from '@/components/tasks/kanban/item';
 
 export const TaskList = () => {
   return (
     <KanbanBoardContainer>
       <KanbanBoard>
-        <KanbanColumn></KanbanColumn>
+        <KanbanColumn>
+          <KanbanItem></KanbanItem>
+        </KanbanColumn>
         <KanbanColumn></KanbanColumn>
       </KanbanBoard>
     </KanbanBoardContainer>
